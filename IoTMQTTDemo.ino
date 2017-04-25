d123480eec7b9423160f358562ef79239d8cfe5f
Mark Radbourne <markrad@msn.com>
2017-04-24 22:26:21 -0700

    Add LED feedback; Add AP configuration; Clean up code

--- IoTMQTTDemo.ino
+++ IoTMQTTDemo.ino
@@ -1,5 +1,10 @@
+#include <FS.h>
+
 #include <MQTTClient.h>
 
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
@@ -23,23 +28,23 @@
  * 
  */
 
-// Next five variables need to have your appropriate values added
-const String SSID = "<Wi-Fi SSID>";
-const String PASSWORD = "<Wi-Fi Password>";
-// Connection string int the form HostName=myhost.azure-devices.net;DeviceId=mydevice;SharedAccessKey=sharedaccesskey
-// This can be acquired from the Azure Portal or the Device Explorer
-const String CONNECTIONSTRING = "<Connection String>";
 const uint INTERVAL = 20;             // SAS token validity period in minutes
 const int REFRESHPERCENTAGE = 80;     // Percentage of SAS token validity used when reauthentication is attempted
+const int MISSEDTOOMANY = 10;         // Reconnect to MQTT when consecutively missed this many messages 
+const int BLINKER = BUILTIN_LED;
+const int BLINKTIME = 700;
+const int BLINKOFF = HIGH;
+const int BLINKON = LOW;              // Set to BLINKOFF to disable LED
 
 WiFiUDP ntpUDP;
 WiFiClientSecure net;
 NTPClient timeClient(ntpUDP);
 MQTTClient client;
-ConnectionStringHelper *csHelper;
+ConnectionStringHelper *csHelper = NULL;
 String pubTopic;
 String subTopic;
 int32_t refreshTime = 0;
+bool shouldSaveConfig = false;
 
 //
 // Included for debug print of binary arrays
@@ -54,22 +59,128 @@ void printBinary(uint8_t *data, size_t dataLength)
   Serial.println();
 }
 
-void setupWiFi(const String ssid, const String password)
+//void softwareReboot()
+//{
+//  Serial.println("Rebooting...");
+//  wdt_enable(WDTO_250MS);
+//  while(true);
+//}
+
+void saveConfigCallback() 
 {
-  Serial.print("Connecting to SSID ");
-  Serial.println(ssid);
+  Serial.println("Config needs to be saved");
+  shouldSaveConfig = true;
+}
 
-  WiFi.begin(ssid.c_str(), password.c_str());
+void configModeCallback(WiFiManager *wifiManager)
+{
+  Serial.println("Entering configuration mode");
+  Serial.println(WiFi.softAPIP());
+}
 
-  while ( WiFi.status() != WL_CONNECTED ) 
+void setupWiFi()
+{
+  bool forceConfig = false;
+  Serial.println("Mounting SPIFFS");
+
+  if (SPIFFS.begin())
   {
-    delay ( 500 );
-    Serial.print ( "." );
+    if (SPIFFS.exists("/config.txt"))
+    {
+      Serial.println("Opening config.txt");
+  
+      File configFile = SPIFFS.open("/config.txt", "r");
+  
+      if (configFile)
+      {
+        Serial.println("Found config.txt");
+  
+        size_t size = configFile.size();
+        char *configData = new char[size];
+  
+        configFile.readBytes(configData, size);
+        
+        csHelper = new ConnectionStringHelper(configData);
+        Serial.print("Connection string = ");
+        Serial.println(configData);
+        delete [] configData;
+        configFile.close();
+      }
+      else
+      {
+        Serial.println("Failed to open config.txt");
+        forceConfig = true;   // May not help any
+      }
+    }
+    else
+    {
+      Serial.println("File config.txt not found");
+      forceConfig = true;
+    }
+  }
+  else
+  {
+    Serial.println("Failed to mount SPIFFS");
+    forceConfig = true;
+
   }
 
+  WiFiManagerParameter connectionStringParm("connectionString", "Connection String", NULL, 150);
+  WiFiManager wifiManager;
+
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.addParameter(&connectionStringParm);
+
+  Serial.println("Connecting or configuring");
+
+  bool connected = false;
+  
+  if (forceConfig)
+  {
+    // Don't bother trying to connect we need more info
+    connected = wifiManager.startConfigPortal("ESP8266Config", "ESP8266");
+  }
+  else
+  {
+    connected = wifiManager.autoConnect("ESP8266Config", "ESP8266");
+  }
+
+  if (!connected)
+  {
+    Serial.println("Failed to connect to Wi-Fi");
+    ESP.reset();
+  }
+
+  Serial.println(WiFi.SSID());
+  
   Serial.println();
   Serial.print("IP Address = ");
   Serial.println(WiFi.localIP());
+
+  if (shouldSaveConfig)
+  {
+    Serial.println("Saving config.txt");
+
+    File configFile = SPIFFS.open("/config.txt", "w");
+
+    if (!configFile)
+    {
+      Serial.println("Failed to open config.txt");
+    }
+
+    Serial.println(configFile.write((const uint8_t *)connectionStringParm.getValue(), strlen(connectionStringParm.getValue()) + 1));
+    configFile.close();
+
+    // Sanity check
+    if (!SPIFFS.exists("/config.txt"))
+    {
+      Serial.println("WTF... where is my new file?");
+    }
+
+    csHelper = new ConnectionStringHelper(connectionStringParm.getValue());
+    Serial.println("Saved config.txt");
+  }
 }
 
 void setupMQTT(uint interval)
@@ -84,14 +195,37 @@ void setupMQTT(uint interval)
   
   csHelper->generatePassword(mqttServer, mqttClientId, csHelper->getKeywordValue("SharedAccessKey"), epochTime, mqttPassword);
   refreshTime = now + (interval * 60 * REFRESHPERCENTAGE / 100);
+  Serial.print("Time is now ");
+  Serial.println(now, DEC);
+  Serial.print("SAS token expires at ");
+  Serial.println(epochTime, DEC);
+  Serial.print("Will refresh at ");
+  Serial.println(refreshTime, DEC);
+  
+  Serial.print("MQTT_Server=");
+  Serial.println(mqttServer);
+  Serial.print("MQTT_ClientId=");
+  Serial.println(mqttClientId);
+  Serial.print("MQTT_UserName=");
+  Serial.println(mqttUserName);
+  Serial.print("MQTT_Password=");
+  Serial.println(mqttPassword);
 
   uint8_t ret;
   client.begin(mqttServer.c_str(), 8883, net);
 
+  int counter = 20;
+  
+  // If we can't connect in 20 attempts then reboot
   while (!client.connect(mqttClientId.c_str(), mqttUserName.c_str(), mqttPassword.c_str()))
   {
     Serial.print(".");
     delay(1000);    
+
+    if (0 >= --counter)
+    {
+      ESP.reset();
+    }
   }
 
   Serial.println("MQTT connected");
@@ -111,12 +245,19 @@ void setup()
   Serial.println();
   Serial.println("Start Test");
 
-  csHelper = new ConnectionStringHelper(CONNECTIONSTRING);
+  pinMode(BLINKER, OUTPUT);
+  digitalWrite(BLINKER, BLINKOFF);
 
-  setupWiFi(SSID, PASSWORD);
+  setupWiFi();
 
   timeClient.begin();
-  timeClient.update();
+
+  // Cannot continue until we have a clock update
+  while (!timeClient.update())
+  {
+    Serial.println("Initial clock update failed");
+    delay(250);
+  }
   delay(1000);
   
   setupMQTT(INTERVAL);
@@ -126,9 +267,16 @@ void loop() {
 
   static uint32_t counter = 0;
   static uint32_t lastMsg = 0;
+  static uint32_t missedMessageCount = 0;
+  static uint32_t ledOffAt = 0;
   uint32_t currMillis;
   
-  timeClient.update();
+  while (!timeClient.update())
+  {
+    Serial.println("NTP update failed");
+    delay(250);
+  }
+  
   client.loop();
 
   // Refresh the SAS token if necessary
@@ -136,12 +284,18 @@ void loop() {
   {
     Serial.println("Reconnecting to refresh SAS token");
 
-    // By hacking the MQTT library here we could improve reauthentication performance
     client.disconnect();
     setupMQTT(INTERVAL);
+    missedMessageCount = 0;
   }
 
   currMillis = millis();
+
+  if (ledOffAt && currMillis >= ledOffAt)
+  {
+    digitalWrite(BLINKER, BLINKOFF);
+    ledOffAt = 0;
+  }
   
   // Check for wrap (around every 50 days)
   if (currMillis < lastMsg)
@@ -151,10 +305,25 @@ void loop() {
   if (currMillis - lastMsg > 3000)
   {
     lastMsg = currMillis;
-    String msg = "{ \"counter\": " + String(counter++) + ", \"millis\": " + String(currMillis) + " }";
+    String msg = "{ \"counter\": " + String(counter++) + ", \"epoch\": " + String(timeClient.getEpochTime()) + ", \"millis\": " + String(currMillis) + " }";
     
     if (!client.publish(pubTopic.c_str(), msg.c_str()))
+    {
       Serial.println("Publish failed");
+
+      if (++missedMessageCount > MISSEDTOOMANY)
+      {
+        missedMessageCount = 0;
+        client.disconnect();
+        setupMQTT(INTERVAL);
+      }
+    }
+    else
+    {
+      missedMessageCount = 0;
+      digitalWrite(BLINKER, BLINKON);
+      ledOffAt = currMillis + BLINKTIME;
+    }
   }
 }
 
